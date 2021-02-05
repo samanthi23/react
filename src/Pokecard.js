ef6558c934e3787fbfb7ebb6c9fdc9e6b219f872
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2021-02-05 20:59:02 +0000

    Added POKE API

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon';
+
 
 class Pokecard extends Component {
     render() {
