f3cf9a3654c2af30f40f77fcd850dc2f87e862f9
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2021-03-05 20:51:12 +0000

    made cards look like polaroids

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -18,7 +18,9 @@ class Pokecard extends Component {
             <h1 className="Pokecard-title">
                 {this.props.name}
             </h1>
-            <img src ={imgSrc} alt={this.props.name} />
+            <div className="Pokecard-image">
+                <img src ={imgSrc} alt={this.props.name} />
+            </div>
             <div className="Pokecard-data">
                 Type: {this.props.type}
             </div>
